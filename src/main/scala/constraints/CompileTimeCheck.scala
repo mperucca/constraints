362da8a3e559fff026eb1c3816c3d8f486930e83
Mike <michaelperucca@gmail.com>
2023-01-07 20:41:49 -0700

    Add more documentation

--- src/main/scala/constraints/CompileTimeCheck.scala
+++ src/main/scala/constraints/CompileTimeCheck.scala
@@ -2,11 +2,37 @@ package constraints
 
 import quoted.*
 
+/**
+ * A type class for constraint checking that is performed at compile time
+ * @tparam A the constraint to check
+ */
 trait CompileTimeCheck[-A]:
+
+  /**
+   * The check to run at compile time.
+   * If the inlining process resolves to a literal false, null, or true, then constraints can compose.
+   * @return false if the constraint doesn't hold,
+   *         null if constraint satisfaction cannot be determined, and
+   *         true if the constraint holds
+   */
   inline def valid: false | Null | true
 
+/**
+ * Utility methods and type class instances for provided combinators
+ */
 object CompileTimeCheck:
 
+  /**
+   * Utility method for [[CompileTimeCheck]] macro implementations that evaluates a constraint by attempting to
+   * to extract a constant value from type [[A]] and then calling the provided [[RuntimeCheck]] with the value
+   * @param runtimeCheck a function returning a [[RuntimeCheck]] when given the value extracted from [[A]]
+   * @param Quotes performs operations on expressions
+   * @tparam A the type from which to attempt extracting a value
+   * @return an expression that is
+   *         false if the extracted value fails the runtime check,
+   *         null if the value cannot be extracted, and
+   *         true if the extracted value passes the runtime check
+   */
   def fromRuntimeCheckOnConstant[A: Type](runtimeCheck: (a: A) => RuntimeCheck[Nothing])(using Quotes): Expr[false | Null | true] =
     valueOfConstantRecursive[A] match
       case None => '{null}
@@ -15,25 +41,68 @@ object CompileTimeCheck:
         then '{true}
         else '{false}
 
+  /**
+   * A check that inlines false (constraint satisfaction invalidated)
+   */
   private object False extends CompileTimeCheck[Any]:
     override inline def valid: false = false
 
+  /**
+   * A check that inlines null (representing constraint satisfaction being undetermined)
+   */
   private object Unknown extends CompileTimeCheck[Any]:
     override inline def valid: Null = null
 
+  /**
+   * A check that inlines true (constraint satisfaction validated)
+   */
   private object True extends CompileTimeCheck[Any]:
     override inline def valid: true = true
 
+  /**
+   * The type class instance for false that invalidates constraints
+   * @return the type class instance [[False]]
+   */
   transparent inline given falsehood: CompileTimeCheck[false] = False
+
+  /**
+   * The type class instance for [[Null]] that leaves constraint satisfaction unknown
+   * @return the type class instance [[Unknown]]
+   */
   transparent inline given unknown: CompileTimeCheck[Null] = Unknown
+
+  /**
+   * The type class instance for true that validates constraints
+   * @return the type class instance for [[True]]
+   */
   transparent inline given truth: CompileTimeCheck[true] = True
 
-  transparent inline given[A](using inline a: CompileTimeCheck[A]): CompileTimeCheck[not[A]] =
+  /**
+   * Type class instance of [[CompileTimeCheck]] for [[not]]
+   * @param a the [[CompileTimeCheck]] instance to negate
+   * @tparam A the constraint to negate
+   * @return a [[CompileTimeCheck]] for the negation of [[A]]
+   *         [[not]] on false becomes true
+   *         [[not]] on unknown stays unknown
+   *         [[not]] on true becomes false
+   */
+  transparent inline given [A](using inline a: CompileTimeCheck[A]): CompileTimeCheck[not[A]] =
     inline a.valid match
       case false => True
       case null => Unknown
       case true => False
 
+  /**
+   * Type class instance of [[CompileTimeCheck]] for [[and]]
+   * @param a the first [[CompileTimeCheck]] of the conjunction
+   * @param b the second [[CompileTimeCheck]] of the conjunction
+   * @tparam A the first constraint of the conjunction
+   * @tparam B the second constraint of the conjunction
+   * @return a [[CompileTimeCheck]] for the conjunction of [[A]] and [[B]]
+   *         either being false results in false
+   *         neither being false and at least one being unknown results in unknown
+   *         both being true results in true
+   */
   transparent inline given [A, B](
     using inline a: CompileTimeCheck[A], inline b: CompileTimeCheck[B]
   ): CompileTimeCheck[A and B] =
@@ -47,6 +116,17 @@ object CompileTimeCheck:
         case null => Unknown
         case true => True
 
+  /**
+   * Type class instance of [[CompileTimeCheck]] for [[or]]
+   * @param a the first [[CompileTimeCheck]] of the inclusive disjunction
+   * @param b the second [[CompileTimeCheck]] of the inclusive disjunction
+   * @tparam A the first constraint of the inclusive disjunction
+   * @tparam B the second constraint of the inclusive disjunction
+   * @return a [[CompileTimeCheck]] for the inclusive disjunction of [[A]] or [[B]]
+   *         either being true results in true
+   *         neither being true and at least one being unknown results in unknown
+   *         both being false results in false
+   */
   transparent inline given [A, B](
     using inline a: CompileTimeCheck[A], inline b: CompileTimeCheck[B]
   ): CompileTimeCheck[A or B] =
@@ -60,6 +140,16 @@ object CompileTimeCheck:
         case true => True
       case true => True
 
+  /**
+   * Type class instance of [[CompileTimeCheck]] for [[xor]]
+   * @param a the first [[CompileTimeCheck]] of the exclusive disjunction
+   * @param b the second [[CompileTimeCheck]] of the exclusive disjunction
+   * @tparam A the first constraint of the exclusive disjunction
+   * @tparam B the second constraint of the exclusive disjunction
+   * @return a [[CompileTimeCheck]] for the exclusive disjunction of [[A]] xor [[B]]
+   *         either being unknown results in unknown
+   *         both being known results in false if they are the same and true if they are different
+   */
   transparent inline given [A, B](
     using inline a: CompileTimeCheck[A], inline b: CompileTimeCheck[B]
   ): CompileTimeCheck[A xor B] =
--- src/main/scala/constraints/Constrained.scala
+++ src/main/scala/constraints/Constrained.scala
@@ -2,15 +2,50 @@ package constraints
 
 import scala.collection.IterableOps
 
+/**
+ * A value with an attached constraint
+ * @param value the value constrained by [[P]]
+ * @param trust evidence of the constraint on the [[value]]
+ * @tparam A the type being constrained
+ * @tparam P the constraint
+ */
 class Constrained[+A, P[_ <: A]](val value: A)(val trust: Trust[P[value.type]])
 
+/**
+ * Utility methods for constructing [[Constrained]] values
+ */
 object Constrained:
 
+  /**
+   * Constructs a [[Constrained]] value with nicer inference than the constructor
+   * @note [[P]] cannot have type bounds with this construction method
+   * @param a the value to constrain
+   * @param trust evidence of the constraint
+   * @tparam P the constraint
+   * @return the constrained value
+   */
   def apply[P[_]](a: Any)(trust: Trust[P[a.type]]): Constrained[a.type, P] = new Constrained(a)(trust)
 
+  /**
+   * Constructs a function that creates [[Constrained]] values with nicer inference than the constructor
+   * @param a the value to constrain
+   * @return a function that creates a constrained value when supplied constraint evidence
+   */
   def apply(a: Any): [P[_ <: a.type]] => Trust[P[a.type]] => Constrained[a.type, P] =
     [P[_ <: a.type]] => (trust: Trust[P[a.type]]) => new Constrained[a.type, P](a)(trust)
 
+  /**
+   * Partitions an iterable into a [[Tuple2]] where
+   * the 1st item contains values and inverse [[Trust]] evidences that the inverse of constraint [[P]] holds
+   * the 2nd item contains values and [[Trust]] evidences that the constraint [[P]] holds
+   * @param iterable the values to check constraint [[P]] on
+   * @param runtimeCheck the runtime check constraint to perform
+   * @param I[A] <:< IterableOps[A, I, I[A]] evidence that the values can be iterated over
+   * @tparam I the iterable type
+   * @tparam A the value type inside the iterable
+   * @tparam P the constraint to check on each value
+   * @return a tuple containing
+   */
   def partition[I[_], A, P[_]](iterable: I[A])(runtimeCheck: (a: A) => RuntimeCheck[P[a.type]])(
     using I[A] <:< IterableOps[A, I, I[A]]
   ): (I[A Constrained Inverse[P]], I[A Constrained P]) =
