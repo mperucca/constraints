e873677252656e88adde7b6333b81016807741d8
Mike <michaelperucca@gmail.com>
2023-05-26 23:17:49 -0600

    Simpler whitebox implementation

--- src/test/scala/Fraction.scala
+++ src/test/scala/Fraction.scala
@@ -1,17 +1,17 @@
 import constraints.*
 
-type Fraction = Fraction.BlackBox
+class Fraction(val numerator: Int, val denominator: Int)(
+  val nonZeroDenominator: Guarantee[denominator.type !== 0]
+)
 
 object Fraction:
 
-  type BlackBox = Fraction.WhiteBox[Int, Int]
+  type WhiteBox[+N <: Int, +D <: Int] = Fraction { val numerator: N; val denominator: D }
 
-  class WhiteBox[+N <: Int, +D <: Int](val numerator: N, val denominator: D)(
-    val nonZeroDenominator: Guarantee[denominator.type !== 0]
-  )
-
-  def apply(numerator: Int, denominator: Int)(guarantee: Guarantee[denominator.type !== 0]) =
-    new Fraction.WhiteBox[numerator.type, denominator.type](numerator, denominator)(guarantee)
+  def apply(numerator: Int, denominator: Int)(
+    guarantee: Guarantee[denominator.type !== 0]
+  ): Fraction.WhiteBox[numerator.type, denominator.type] =
+    new Fraction(numerator, denominator)(guarantee).asInstanceOf
 
   type Numerator[F] = F match { case Fraction.WhiteBox[n, ?] => n }
   type Denominator[F] = F match { case Fraction.WhiteBox[?, d] => d }
